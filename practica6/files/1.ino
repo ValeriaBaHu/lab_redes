48d60cb01ee93d47361462f4208511159ccc4d5a
Valeria <195409@iberopuebla.mx>
2025-05-12 20:08:59 -0600

    pract6

---
+++ practica6/files/1.ino
@@ -0,0 +1,10 @@
+int LED=23;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(LED, HIGH);
+}
